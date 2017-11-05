6f9158134d0cea1b118f6f198a7869a792d6957e
wuyl <wyl_xlj@163.com>
2017-11-05 22:31:22 +0800

    补提 zuul项目

---
+++ cloudshopper-zuul/src/main/java/org/star4j/cloudshopper/ZuulApplication.java
@@ -0,0 +1,15 @@
+package org.star4j.cloudshopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
